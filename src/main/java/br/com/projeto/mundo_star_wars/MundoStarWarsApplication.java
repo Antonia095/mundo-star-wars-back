3e5e04bcede79885659c8075a48e5426fec5966d
Antonia Maciel <antonia.josy95@gmail.com>
2025-09-29 11:39:26 -0300

    estruturação do projeto

---
+++ src/main/java/br/com/projeto/mundo_star_wars/MundoStarWarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.mundo_star_wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundoStarWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundoStarWarsApplication.class, args);
+	}
+
+}
